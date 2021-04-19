d3e17ac57e7b5e215012d693818bcd58ecf80d73
Eynman <71591468+Eynman@users.noreply.github.com>
2021-04-19 15:34:04 +0300

    Add files via upload
    The basic structure of a website that can be done with PHP has been created. It'll continue step by step.

---
+++ app/config.php
@@ -0,0 +1,14 @@
+<?php
+
+define('PATH', realpath('.'));
+define('SUBFOLDER', true);
+define('URL', 'http://localhost/Proje');
+
+return [
+    'db' => [
+        'name' => 'Project',
+        'host' => 'localhost',
+        'user' => 'root',
+        'pass' => ''
+    ]
+];
---
+++ app/controller/404.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "controls not found!";
